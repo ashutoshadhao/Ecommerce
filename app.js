42ab103784ef3df8cc99389ec5bc9afffab20e07
Ashutosh Adhao <adhaoashutosh@gmail.com>
2023-02-02 01:26:46 +0530

    NULL 3

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.get("*/", (req, res) => {
   console.log("Hello i am hear in frontend ")
-  res.sendFile(path.resolve(__dirname, `./frontend/build/index.html`));
+  res.sendFile(path.join(__dirname, `./frontend/build/index.html`));
 });
 // middleware for Error
 app.use(errorMiddleware);
